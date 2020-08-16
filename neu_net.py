0815439fa2514a669a6b267ca28ba461999dbd9e
xram255 <roshan.cs790@gmail.com>
2020-08-16 19:26:03 +0530

    jupyter notebook added

--- neu_net.py
+++ neu_net.py
@@ -12,25 +12,26 @@ streetlights = np.array([[0, 0, 1],
 
 walk_vs_stop = np.array([0, 1, 0, 1, 1, 0])
 
+'''
 input = streetlights[2]
 print(weights)
 print(input.dot(weights))
-
+'''
 
 for iteration in range (40):
     error_for_all_lights = 0
     for row_index in range (len(walk_vs_stop)):
         input = streetlights[row_index]
         goal_prediction = walk_vs_stop[row_index]
-
+        print(error_for_all_lights)
         prediction = input.dot(weights)
         error = (prediction -goal_prediction) ** 2 #square error
         error_for_all_lights += error
+        print("in", error_for_all_lights)
+        delta = prediction - goal_prediction #difference between actual to prediction
+        weights = weights -(alpha * input * delta) #weights update
 
-        delta = prediction - goal_prediction
-        weights = weights -(alpha * input * delta)#weights update
-
-        print("prediction: ", prediction)
+        #print("prediction: ", prediction)
 
     print("weights: ", weights)
     print("Error: ", prediction)
